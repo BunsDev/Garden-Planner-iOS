e2a50e41dd7e43f89b505e464b67ca278bcc7dc0
sfortier32 <sfortier@umass.edu>
2022-06-20 18:38:18 -0400

    Initial Commit

---
+++ Garden Planner 2/Garden_Planner_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Garden_Planner_2App.swift
+//  Garden Planner 2
+//
+//  Created by Sophia Fortier on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Garden_Planner_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
